00b24ad426aa0cf47e8a11ebc0068c526f21c10f
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-12-01 18:48:18 +0530

    Added:All Entries From DB

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,7 +1,33 @@
 package com.bridgelabz;
 
+
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBookMain {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the addressbook program");
+
+        boolean exit = false;
+        while (!exit) {
+            System.out.println(" Press\n 1 ->  Retrieve data\n 2 -> Exit");
+            switch (scanner.nextInt()) {
+                case 1:
+                    retrieveData();
+                    break;
+                case 2:
+                    exit = true;
+            }
+        }
+    }
+
+    private static void retrieveData() {
+        AddressBookRepo addressBookRepo = new AddressBookRepo();
+        List<Contacts> employeeInfoList = addressBookRepo.retrieveData();
+        for (Contacts employee : employeeInfoList
+        ) {
+            System.out.println(employee + "\n");
+        }
     }
 }
